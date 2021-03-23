4eddffc37bfcbee4ba75e0d2d85d1497f4087706
yangkai <809432638@qq.com>
2021-03-23 17:41:47 +0800

    commit

---
+++ hbase/src/main/java/util/HBaseInfo.java
@@ -0,0 +1,44 @@
+package util;
+
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class HBaseInfo {
+    public static byte[] ROW1 = Bytes.toBytes("row1");
+    public static byte[] ROW2 = Bytes.toBytes("row2");
+    public static byte[] ROW3 = Bytes.toBytes("row3");
+    public static byte[] ROW4 = Bytes.toBytes("row4");
+    public static byte[] ROW5 = Bytes.toBytes("row5");
+    public static byte[] ROW6 = Bytes.toBytes("row6");
+    public static byte[] ROW7 = Bytes.toBytes("row7");
+    public static byte[] ROW8 = Bytes.toBytes("row8");
+    public static byte[] ROW9 = Bytes.toBytes("row9");
+    public static byte[] CF1 = Bytes.toBytes("cf1");
+    public static byte[] CF2 = Bytes.toBytes("cf2");
+    public static byte[] F1C1 = Bytes.toBytes("f1c1");
+    public static byte[] F1C2 = Bytes.toBytes("f1c2");
+    public static byte[] F1C3 = Bytes.toBytes("f1c3");
+    public static byte[] F1C4 = Bytes.toBytes("f1c4");
+    public static byte[] F1C5 = Bytes.toBytes("f1c5");
+    public static byte[] F1C6 = Bytes.toBytes("f1c6");
+    public static byte[] F1C7 = Bytes.toBytes("f1c7");
+    public static byte[] F1C8 = Bytes.toBytes("f1c8");
+    public static byte[] F1C9 = Bytes.toBytes("f1c9");
+    public static byte[] F2C1 = Bytes.toBytes("f2c1");
+    public static byte[] F2C2 = Bytes.toBytes("f2c2");
+    public static byte[] F2C3 = Bytes.toBytes("f2c3");
+    public static byte[] F2C4 = Bytes.toBytes("f2c4");
+    public static byte[] F2C5 = Bytes.toBytes("f2c5");
+    public static byte[] F2C6 = Bytes.toBytes("f2c6");
+    public static byte[] F2C7 = Bytes.toBytes("f2c7");
+    public static byte[] F2C8 = Bytes.toBytes("f2c8");
+    public static byte[] F2C9 = Bytes.toBytes("f2c9");
+    public static byte[] V1 = Bytes.toBytes("v1");
+    public static byte[] V2 = Bytes.toBytes("v2");
+    public static byte[] V3 = Bytes.toBytes("v3");
+    public static byte[] V4 = Bytes.toBytes("v4");
+    public static byte[] V5 = Bytes.toBytes("v5");
+    public static byte[] V6 = Bytes.toBytes("v6");
+    public static byte[] V7 = Bytes.toBytes("v7");
+    public static byte[] V8 = Bytes.toBytes("v8");
+    public static byte[] V9 = Bytes.toBytes("v9");
+}
